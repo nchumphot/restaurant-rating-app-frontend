28d91eb73c6a666438df9c339a643f696d081afc
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-18 20:43:09 +0000

    Fetch restaurants in useEffect and passed props for AllRestaurants

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { useState } from 'react';
-import { AllRestaurants } from './components/AllRestaurants';
-import { PageHeader } from './components/PageHeader';
-import './css/App.css';
-import { IRestaurant } from './utils/IRestaurant';
+import { useEffect, useState } from "react";
+import { AllRestaurants } from "./components/AllRestaurants";
+import { PageHeader } from "./components/PageHeader";
+import "./css/App.css";
+import { fetchData } from "./utils/fetchData";
+import { IRestaurant } from "./utils/IRestaurant";
 
 function App(): JSX.Element {
   const [allRestaurants, setAllRestaurants] = useState<Array<IRestaurant>>([]);
+  useEffect(() => {
+    fetchData("http://localhost:4000/restaurants", setAllRestaurants);
+  }, []);
   return (
     <>
       <PageHeader />
-      <AllRestaurants />
+      <AllRestaurants restaurants={allRestaurants} />
     </>
-  )
+  );
 }
 
 export default App;
