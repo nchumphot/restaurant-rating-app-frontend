c2cabc42b4fcadc54690cb967f478d0d2b6eaf7e
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-18 21:53:03 +0000

    Successfully prints comment cards but errors on printing address

--- src/components/SelectedRestaurant.tsx
+++ src/components/SelectedRestaurant.tsx
@@ -1,23 +1,40 @@
 import { useEffect, useState } from "react";
+import { IRestaurant } from "../interfaces/IRestaurant";
 import { IRestaurantReview } from "../interfaces/IRestaurantReview";
-import { fetchOneRestaurant } from "../utils/fetchOneRestaurant";
+import { fetchComments } from "../utils/fetchComments";
 import { CommentCard } from "./CommentCard";
 
 export function SelectedRestaurant(props: { id: number }): JSX.Element {
   const [reviews, setReviews] = useState<IRestaurantReview[]>([]);
+  const [restaurant, setRestaurant] = useState<IRestaurant>({
+    id: 0,
+    name: "testing",
+    street: "string",
+    city: "string",
+    postcode: "string",
+  });
   useEffect(() => {
     const url = `http://localhost:4000/restaurants/${props.id}`;
     console.log(url);
-    fetchOneRestaurant(url, setReviews);
+    fetchComments(url, setRestaurant, setReviews);
   }, [props.id]);
   console.log(props.id);
+  console.log(restaurant);
   console.log(reviews);
   return (
-    <section>
-      <h2>Selected restaurant</h2>
-      {reviews.map((item) => {
-        <CommentCard review={item} />;
-      })}
-    </section>
+    <>
+      {reviews.length > 0 && (
+        <>
+          <h2>Your selection</h2>
+          <h3>{restaurant.name}</h3>
+          <small>{`${restaurant.street}, ${restaurant.city} ${restaurant.postcode}`}</small>
+          <p>
+            {reviews.map((review) => (
+              <CommentCard key={review.id} review={review} />
+            ))}
+          </p>
+        </>
+      )}
+    </>
   );
 }
