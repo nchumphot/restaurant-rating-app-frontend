157c8c5d05d90f333cb36f256ab8ec58a52a249e
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-18 20:44:51 +0000

    Completed the fetch + setState function

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,3 +1,19 @@
-export function fetchData(url: string,) {
-    fetch(url).then((response) => response.json()).then((data) => console.log(data))
+import { IRestaurant } from "./IRestaurant";
+
+export function fetchData(
+  url: string,
+  setState: React.Dispatch<React.SetStateAction<IRestaurant[]>>
+) {
+  fetch(url)
+    .then((response) => response.json())
+    .then(
+      (jsonBody: {
+        status: string;
+        message: string;
+        response: IRestaurant[];
+      }) => {
+        console.log(jsonBody);
+        setState(jsonBody.response);
+      }
+    );
 }
