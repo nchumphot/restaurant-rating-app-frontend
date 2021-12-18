c40a01b1b0f631b498083e3ac546fe0c2b9bb350
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-18 20:43:29 +0000

    Use .map method to show all restaurants

--- src/components/AllRestaurants.tsx
+++ src/components/AllRestaurants.tsx
@@ -1,15 +1,16 @@
+import { IRestaurant } from "../utils/IRestaurant";
 import { RestaurantCard } from "./RestaurantCard";
 
-
-export function AllRestaurants(): JSX.Element {
-    return (
-        <section>
-            <h2>All restaurants</h2>
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-        </section>
-    );
+export function AllRestaurants(props: {
+  restaurants: IRestaurant[];
+}): JSX.Element {
+  console.log(props.restaurants);
+  return (
+    <section>
+      <h2>All restaurants</h2>
+      {props.restaurants.map((item) => (
+        <RestaurantCard key={item.id} {...item} />
+      ))}
+    </section>
+  );
 }
