e9eca55d3cdc4514deb558934b66fe0501eec41d
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-18 20:43:59 +0000

    Prints restaurant information according to props

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,8 +1,13 @@
-export function RestaurantCard(): JSX.Element {
-    return (
-        <>
-<h3>Restaurant name</h3>
-<p>Restaurant address</p>
-</>
-    );
+import { IRestaurant } from "../utils/IRestaurant";
+
+export function RestaurantCard(props: IRestaurant): JSX.Element {
+  return (
+    <>
+      <h3>{props.name}</h3>
+      <small>{`${props.street}, ${props.city} ${props.postcode}`}</small>
+      <br />
+      <br />
+      <button>See more</button>
+    </>
+  );
 }
