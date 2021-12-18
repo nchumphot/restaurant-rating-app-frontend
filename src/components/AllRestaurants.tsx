ec661106264f74f51c7d1b387d9263c660d20988
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-18 21:23:31 +0000

    Passing setSelection into RestaurantCard

--- src/components/AllRestaurants.tsx
+++ src/components/AllRestaurants.tsx
@@ -1,15 +1,20 @@
-import { IRestaurant } from "../utils/IRestaurant";
+import { IRestaurant } from "../interfaces/IRestaurant";
 import { RestaurantCard } from "./RestaurantCard";
 
 export function AllRestaurants(props: {
   restaurants: IRestaurant[];
+  setSelection: React.Dispatch<React.SetStateAction<number>>;
 }): JSX.Element {
   console.log(props.restaurants);
   return (
     <section>
       <h2>All restaurants</h2>
       {props.restaurants.map((item) => (
-        <RestaurantCard key={item.id} {...item} />
+        <RestaurantCard
+          key={item.id}
+          restaurant={item}
+          setSelection={props.setSelection}
+        />
       ))}
     </section>
   );
