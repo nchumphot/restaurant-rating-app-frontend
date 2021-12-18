0b9ab83abece4444b10b08f71e1f87b9faff7ab2
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-18 21:52:25 +0000

    Formatted with Prettier

--- src/interfaces/IRestaurant.ts
+++ src/interfaces/IRestaurant.ts
@@ -1,7 +1,7 @@
 export interface IRestaurant {
-    id: number,
-    name: string,
-    street: string,
-    city: string,
-    postcode: string
+  id: number;
+  name: string;
+  street: string;
+  city: string;
+  postcode: string;
 }
