f4b95c44e954139d3da81f11eb2d5294319407c9
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-18 21:23:01 +0000

    Passing props into AllRestaurants and added SelectedRestaurant

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { useEffect, useState } from "react";
 import { AllRestaurants } from "./components/AllRestaurants";
 import { PageHeader } from "./components/PageHeader";
+import { SelectedRestaurant } from "./components/SelectedRestaurant";
 import "./css/App.css";
-import { fetchData } from "./utils/fetchData";
-import { IRestaurant } from "./utils/IRestaurant";
+import { fetchAllRestaurants } from "./utils/fetchAllRestaurants";
+import { IRestaurant } from "./interfaces/IRestaurant";
 
 function App(): JSX.Element {
-  const [allRestaurants, setAllRestaurants] = useState<Array<IRestaurant>>([]);
+  const [allRestaurants, setAllRestaurants] = useState<IRestaurant[]>([]);
+  const [selection, setSelection] = useState<number>(0);
   useEffect(() => {
-    fetchData("http://localhost:4000/restaurants", setAllRestaurants);
+    fetchAllRestaurants("http://localhost:4000/restaurants", setAllRestaurants);
   }, []);
   return (
     <>
       <PageHeader />
-      <AllRestaurants restaurants={allRestaurants} />
+      <AllRestaurants
+        restaurants={allRestaurants}
+        setSelection={setSelection}
+      />
+      <h1>============================================</h1>
+      {selection !== 0 && <SelectedRestaurant id={selection} />}
     </>
   );
 }
